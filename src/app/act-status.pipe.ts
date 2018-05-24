493151cae2e3d5e8aa1a79b320f84feb08823065
Anahí DS <ads_94_@hotmail.com>
2018-05-24 09:01:58 -0700

    se arreglo lo del txtsearch en Health Care

--- src/app/act-status.pipe.ts
+++ src/app/act-status.pipe.ts
@@ -5,8 +5,28 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ActStatusPipe implements PipeTransform {
 
-  public transform(value, keys: string, term: string) {
+  /*public transform(value, keys: string, term: string) {
     if (!term) return value;
     return (value || []).filter((item) => keys.split(',').some(key => item.hasOwnProperty(key) && new RegExp(term, 'gi').test(item[key])));
+  }*/
+
+  transform(items: any, filter: any, isAnd: boolean): any {
+    if (filter && Array.isArray(items)) {
+      let filterKeys = Object.keys(filter);
+      if (isAnd) {
+        return items.filter(item =>
+            filterKeys.reduce((memo, keyName) =>
+                (memo && new RegExp(filter[keyName], 'gi').test(item[keyName])) || filter[keyName] === "", true));
+      } else {
+        return items.filter(item => {
+          return filterKeys.some((keyName) => {
+            console.log(keyName);
+            return new RegExp(filter[keyName], 'gi').test(item[keyName]) || filter[keyName] === "";
+          });
+        });
+      }
+    } else {
+      return items;
+    }
   }
 }
