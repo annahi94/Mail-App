b88bed85014ecfef2b3bc9edadaee72bb5944d77
Anahí DS <ads_94_@hotmail.com>
2018-05-28 10:46:33 -0700

    se arreglo problema del pipe que tenia conflicto con el tiempo real

--- src/app/act-status.pipe.ts
+++ src/app/act-status.pipe.ts
@@ -1,7 +1,8 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'actStatusPipe'
+  name: 'actStatusPipe',
+  pure: false
 })
 export class ActStatusPipe implements PipeTransform {
 
